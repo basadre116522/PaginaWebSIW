9ac71a2e7a7cb320e4dfefe2bbfe19f9cd12dbbd
basadre116522 <basadre.116522@e.unavarra.es>
2020-04-04 14:11:08 +0200

    submenu desplegable

--- frontend/js/startpage.js
+++ frontend/js/startpage.js
@@ -1,3 +1,11 @@
 function desplegar() {
-	element.getElementById("menu2");
+	if (document.getElementById('submenu').style.display = 'none') {
+		document.getElementById('submenu').style.display = 'block';
+	}
+}
+
+function ocultar() {
+	if (document.getElementById('submenu').style.display = 'block') {
+		document.getElementById('submenu').style.display = 'none';
+	}
 }
