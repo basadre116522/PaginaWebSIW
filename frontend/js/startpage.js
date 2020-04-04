1c6d90735c51136a73164d2a20d97b0b29cb9ead
basadre116522 <basadre.116522@e.unavarra.es>
2020-04-04 18:52:46 +0200

    cambia colores del menu

--- frontend/js/startpage.js
+++ frontend/js/startpage.js
@@ -1,15 +1,25 @@
-function desplegar() {
+function desplegar(id) {
 	if (document.getElementById('submenu').style.display = 'none') {
 		document.getElementById('submenu').style.display = 'block';
 	}
+	seleccionado(id);
 }
 
-function ocultar() {
+function ocultar(id) {
 	if (document.getElementById('submenu').style.display = 'block') {
 		document.getElementById('submenu').style.display = 'none';
 	}
+	desseleccionado(id);
 }
 
 function seleccionado(id) {
-	
+	if (document.getElementById(id).style.backgroundColor != '#a2b84b') {
+		document.getElementById(id).style.backgroundColor = '#a2b84b';
+	}
+}
+
+function desseleccionado(id) {
+	if (document.getElementById(id).style.backgroundColor != '#809629') {
+		document.getElementById(id).style.backgroundColor = '#809629';
+	}
 }
